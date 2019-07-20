5957791f55371958479b7eae558a702707e8eb9d
obada <obada454u2u@gmail.com>
2019-07-20 05:02:17 +0300

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -0,0 +1,101 @@
+package com.londonappbrewery.bitcointicker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+import com.loopj.android.http.*;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="MainActivity" ;
+    // Constants:
+    // TODO: Create the base URL
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+
+    // Member Variables:
+    TextView mPriceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
+        final Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+
+        // Create an ArrayAdapter using the String array and a spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, R.layout.spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.d(TAG, "onItemSelected: "+adapterView.getItemAtPosition(position));
+                String finalurl= BASE_URL+adapterView.getItemAtPosition(position);
+                Log.d(TAG, "finalurl: "+finalurl);
+                letsDoSomeNetworking(finalurl);
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    // TODO: complete the letsDoSomeNetworking() method
+    private void letsDoSomeNetworking(String url) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler() {
+        @Override
+            public void  onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+               Log.d("bitcoin", "JSON: " + response.toString());
+               try {
+                    String price= response.getString("last");
+                    mPriceTextView.setText(price);
+               }catch (JSONException e){
+                   e.printStackTrace();
+               }
+            }
+
+             @Override
+            public void onFailure(int statusCode,cz.msebera.android.httpclient.Header[] headers, Throwable e, JSONObject response) {
+                 Log.e("ERROR", e.toString());
+              Log.d("bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("bitcoin", "Fail response: " + response);
+
+               }
+        });
+
+    }
+
+}
